591eed602fa6d04b1ce5d274cb5b50cbe54c1c27
Prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2025-06-01 06:08:03 +0000

    DataTypes

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+//console.log(3+3); console.log("prakant sharma");-> code will work but 
+// not a good practice , reduces readability
+
+let name="Prakant sharma" // string
+let age=20; // number()
+let isLoggedin=true;// boolean
+// null=> standalone value
+// undefined
+// symbol=> unique
+// object
+
+console.log(typeof(age));
+
+console.log(typeof(undefined));
+console.log(typeof null);
